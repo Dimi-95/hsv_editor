6e1e0328302f321ff3759df469b69d51955a1e68
Dimi <46501122+Hexad3x@users.noreply.github.com>
2022-07-07 19:38:58 +0200

    Add files via upload

--- hsvce_2.py
+++ hsvce_2.py
@@ -1,51 +1,35 @@
+from tkinter import filedialog
 import cv2 as cv
 import numpy as np
-from windowcapture import WindowCapture
-import win32gui, win32ui, win32con
-
-
-image = cv.imread("C:\\Users\\d3x\\Desktop\\OpenCv_test_ground\\champion_image\\taric.png")
-image_2 = cv.imread("C:\\Users\\d3x\\Desktop\\OpenCv_test_ground\\champion_image\\taric.png")
+from tkinter import *
+import os
+import sys
 
 window_name = "HSV Editor"
-color_win_position = [(350,30), (700,90)]
 hsv_track = {'h_min': 0, 's_min': 0, 'v_min': 0, 'h_max': 179, 's_max': 255, 'v_max': 255,
             's_add' : 0, 's_sub' : 0, 'v_add' : 0, "v_sub" : 0}
 
 
 font = cv.FONT_HERSHEY_SIMPLEX
+current_action = None
 
 
-img = np.zeros((350,700,3), np.uint8)
+img = np.zeros((200,700), np.uint8)
 cv.namedWindow(window_name)
 
-#HSV MIN-MAX 
-img = cv.putText(img, "h_min: ", (10, 30), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "s_min: ", (170, 30), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "v_min: ", (330, 30), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "h_max: ", (10, 60), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "s_max: ", (170, 60), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "v_max: ", (330, 60), font, 0.5, (255,255,255), 1)
+img = cv.putText(img, "Press 's' to take a picture and print the values into a text file ", (10, 30), font, 0.5, (255,255,255), 1)
+img = cv.putText(img, "Press 'q' to quit ", (10, 60), font, 0.5, (255,255,255), 1)
+img = cv.putText(img, "Press 'r' to choose a new image to edit. Don't forget to save. ", (10, 90), font, 0.5, (255,255,255), 1)
+img = cv.putText(img, "Output: ", (10, 120), font, 0.5, (255,255,255), 1)
+img = cv.putText(img, " ", (90, 120), font, 0.5, (255,255,255), 1)
+img = cv.putText(img, "Note: Image(PNG) and text file gets saved to the same folder of the original image ", (10, 150), font, 0.5, (255,255,255), 1)
+img = cv.putText(img, "Made by d3x", (550, 180), font, 0.4, (255,255,255), 1)
 
-#SV ADD/SUB
-img = cv.putText(img, "s_add: ", (10, 90), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "v_add: ", (170, 90), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "s_sub: ", (330, 90), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "v_sub: ", (490, 90), font, 0.5, (255,255,255), 1)
+def update_H_min_value(x):
+    global font, img, current_action
+    img = cv.putText(img,f"{current_action}", (90, 120), font, 0.5, (0,0,0), 1)
+    img = cv.putText(img,f"{x}", (90, 120), font, 0.5, (255,255,255), 1)
 
-#Values
-#HSV MIN-MAX
-img = cv.putText(img, "0", (80, 30), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "0", (240, 30), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "0", (400, 30), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "179", (80, 60), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "255", (240, 60), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "255", (400, 60), font, 0.5, (255,255,255), 1)
-#SV ADD/SUB
-img = cv.putText(img, "0", (80, 90), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "0", (240, 90), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "0", (400, 90), font, 0.5, (255,255,255), 1)
-img = cv.putText(img, "0", (560, 90), font, 0.5, (255,255,255), 1)
 
 def nothing(x):
     pass
@@ -63,88 +47,35 @@ def shift_channel(c, amount):
         c[c > lim] -= amount
     return c
 
-#Update functions
-#MIN Values
-def update_H_min_value(x):
-    global font, img, hsv_track
-    img = cv.putText(img,f"{hsv_track['h_min']}", (80, 30), font, 0.5, (0,0,0), 1)
-    img = cv.putText(img,f"{x}", (80,30), font, 0.5, (255,255,255), 1)
-    hsv_track['h_min'] = x
-
-def update_S_min_value(x):
-    global font, img, hsv_track
-    img = cv.putText(img,f"{hsv_track['s_min']}", (240, 30), font, 0.5, (0,0,0), 1)
-    img = cv.putText(img,f"{x}", (240,30), font, 0.5, (255,255,255), 1)
-    hsv_track['s_min'] = x
-
-def update_V_min_value(x):
-    global font, img, hsv_track
-    img = cv.putText(img,f"{hsv_track['v_min']}", (400, 30), font, 0.5, (0,0,0), 1)
-    img = cv.putText(img,f"{x}", (400,30), font, 0.5, (255,255,255), 1)
-    hsv_track['v_min'] = x
-#MAX Values
-def update_H_max_value(x):
-    global font, img, hsv_track
-    img = cv.putText(img,f"{hsv_track['h_max']}", (80, 60), font, 0.5, (0,0,0), 1)
-    img = cv.putText(img,f"{x}", (80,60), font, 0.5, (255,255,255), 1)
-    hsv_track['h_max'] = x
-
-def update_S_max_value(x):
-    global font, img, hsv_track
-    img = cv.putText(img,f"{hsv_track['s_max']}", (240, 60), font, 0.5, (0,0,0), 1)
-    img = cv.putText(img,f"{x}", (240,60), font, 0.5, (255,255,255), 1)
-    hsv_track['s_max'] = x
-
-def update_V_max_value(x):
-    global font, img, hsv_track
-    img = cv.putText(img,f"{hsv_track['v_max']}", (400, 60), font, 0.5, (0,0,0), 1)
-    img = cv.putText(img,f"{x}", (400,60), font, 0.5, (255,255,255), 1)
-    hsv_track['v_max'] = x
-#ADD and SUB
-def update_S_add_value(x):
-    global font, img, hsv_track
-    img = cv.putText(img,f"{hsv_track['s_add']}", (80, 90), font, 0.5, (0,0,0), 1)
-    img = cv.putText(img,f"{x}", (80,90), font, 0.5, (255,255,255), 1)
-    hsv_track['s_add'] = x
-
-def update_V_add_value(x):
-    global font, img, hsv_track
-    img = cv.putText(img,f"{hsv_track['v_add']}", (240, 90), font, 0.5, (0,0,0), 1)
-    img = cv.putText(img,f"{x}", (240,90), font, 0.5, (255,255,255), 1)
-    hsv_track['v_add'] = x
-
-def update_S_sub_value(x):
-    global font, img, hsv_track
-    img = cv.putText(img,f"{hsv_track['s_sub']}", (400, 90), font, 0.5, (0,0,0), 1)
-    img = cv.putText(img,f"{x}", (400,90), font, 0.5, (255,255,255), 1)
-    hsv_track['s_sub'] = x
-
-def update_V_sub_value(x):
-    global font, img, hsv_track
-    img = cv.putText(img,f"{hsv_track['v_sub']}", (560, 90), font, 0.5, (0,0,0), 1)
-    img = cv.putText(img,f"{x}", (560,90), font, 0.5, (255,255,255), 1)
-    hsv_track['v_sub'] = x
 
 #Setting up the trackers
 
-cv.createTrackbar("h_min", window_name, 0, 179, update_H_min_value)
-cv.createTrackbar("s_min", window_name, 0, 255, update_S_min_value)
-cv.createTrackbar("v_min", window_name, 0, 255, update_V_min_value)
+cv.createTrackbar("h_min", window_name, 0, 179, nothing)
+cv.createTrackbar("s_min", window_name, 0, 255, nothing)
+cv.createTrackbar("v_min", window_name, 0, 255, nothing)
 
-cv.createTrackbar("h_max", window_name, 0, 179, update_H_max_value)
-cv.createTrackbar("s_max", window_name, 0, 255, update_S_max_value)
-cv.createTrackbar("v_max", window_name, 0, 255, update_V_max_value)
+cv.createTrackbar("h_max", window_name, 0, 179, nothing)
+cv.createTrackbar("s_max", window_name, 0, 255, nothing)
+cv.createTrackbar("v_max", window_name, 0, 255, nothing)
 
 cv.setTrackbarPos('h_max', window_name, 179)
 cv.setTrackbarPos('s_max', window_name, 255)
 cv.setTrackbarPos('v_max', window_name, 255)
 
-cv.createTrackbar("s_add", window_name, 0, 255, update_S_add_value)
-cv.createTrackbar("s_sub", window_name, 0, 255, update_S_sub_value)
-cv.createTrackbar("v_add", window_name, 0, 255, update_V_add_value)
-cv.createTrackbar("v_sub", window_name, 0, 255, update_V_sub_value)
+cv.createTrackbar("s_add", window_name, 0, 255, nothing)
+cv.createTrackbar("s_sub", window_name, 0, 255, nothing)
+cv.createTrackbar("v_add", window_name, 0, 255, nothing)
+cv.createTrackbar("v_sub", window_name, 0, 255, nothing)
+
+path = filedialog.askopenfilename(title="Choose your image to edit !")
 
 while(1):
+    
+
+    image = cv.imread(path)
+
+    image_2 = cv.imread(path)
+
     cv.imshow(window_name, img)
     key = cv.waitKey(1) & 0xff
     if key==ord('q'):
@@ -195,12 +126,18 @@ while(1):
 
     image = cv.cvtColor(result, cv.COLOR_HSV2BGR)
     cv.imshow("Editor_View", image)
-    if key==ord('z'):
-        print("Image has been saved")
-        f = open("HSV_Values.txt", "w")
+    if key==ord('s'):
+        name_of_path = path
+        head_tail = os.path.split(name_of_path)
+        f = open(f"{head_tail[1]}_HSV_values.txt", "w")
         f.write(f"h_min = {h_min}, v_min = {v_min},s_min = {s_min}, h_max = {h_max}, v_max = {v_max}, s_max = {s_max}, s_add = {s_add}, s_sub = {s_sub}, v_add {v_add}, v_sub {v_sub}")
         f.close()
-        cv.imwrite("test.png", image )
+        cv.imwrite(f"{head_tail[1]}_HSV.png", image )
+        current_action = "Picture has been saved and text file created !"
+        update_H_min_value(current_action)
+    if key==ord('r'):
+        os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
+
 
 
 
